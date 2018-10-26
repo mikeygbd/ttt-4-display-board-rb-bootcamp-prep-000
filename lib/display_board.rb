0d576d89227e8348ee9ef2c3063d05d728977c10
Michael Soares <mikey.soares@gmail.com>
2018-10-26 00:42:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 start = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(start)
-puts "#{start[0]}|#{start[1]}|#{start[2]}"
+puts " #{start[0]} | #{start[1]} | #{start[2]} "
 puts "-----------"
-puts "#{start[3]}|#{start[4]}|#{start[5]}"
+puts " #{start[3]} | #{start[4]} | #{start[5]} "
 puts "-----------"
-puts "#{start[6]}|#{start[7]}|#{start[8]}"
+puts " #{start[6]} | #{start[7]} | #{start[8]} "
 end
