24d7729fcaff599291faff338fd8953fa2d48608
Michael Soares <mikey.soares@gmail.com>
2018-10-26 00:35:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,11 +8,3 @@ puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 end
-start = [" ", " ", " ", " ", "x", " ", " ", " ", " "]
-def display_board(space = start)
-puts [space[0]|space[1]|space[2]
-puts "-----------"
-puts [space[3]|space[4]|space[5]
-puts "-----------"
-puts [space[0]|space[1]|space[2]
-end
